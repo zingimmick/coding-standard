b3c82430b719f22be6f2a723f07862f3330fc65e
zingimmick <zingimmick@outlook.com>
2020-07-03 15:46:37 +0800

    Bump rector/rector from 0.7.26 to 0.7.41

--- correct/TestClass.php
+++ correct/TestClass.php
@@ -69,4 +69,33 @@ class TestClass implements TestableContract
     {
         return self::A ?? self::C;
     }
+
+    public function longConditionClosingComment(): void
+    {
+        if (random_int(0, 1)) {
+            echo 1;
+            echo 2;
+            echo 3;
+            echo 4;
+            echo 5;
+            echo 6;
+            echo 7;
+            echo 8;
+            echo 9;
+            echo 10;
+            echo 11;
+            echo 12;
+            echo 13;
+            echo 14;
+            echo 15;
+            echo 16;
+            echo 17;
+            echo 18;
+            echo 19;
+            echo 20;
+            echo 21;
+        } else {
+            echo 22;
+        }//end if
+    }
 }
--- wrong/TestClass.php
+++ wrong/TestClass.php
@@ -55,4 +55,33 @@ public $bar;
     {
         return self::A??self::C;
     }
+
+    public function longConditionClosingComment()
+    {
+        if (random_int(0,1)){
+            echo 1;
+            echo 2;
+            echo 3;
+            echo 4;
+            echo 5;
+            echo 6;
+            echo 7;
+            echo 8;
+            echo 9;
+            echo 10;
+            echo 11;
+            echo 12;
+            echo 13;
+            echo 14;
+            echo 15;
+            echo 16;
+            echo 17;
+            echo 18;
+            echo 19;
+            echo 20;
+            echo 21;
+        }else{
+            echo 22;
+        }
+    }
 }
