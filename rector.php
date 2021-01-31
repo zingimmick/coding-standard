152a45de8840e8132dba6157ba82576ac36958bd
zingimmick <zingimmick@outlook.com>
2021-01-31 15:40:39 +0800

    Remove deprecated rector set

--- rector.php
+++ rector.php
@@ -43,7 +43,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters->set(
         Option::SETS,
         [
-            SetList::ACTION_INJECTION_TO_CONSTRUCTOR_INJECTION,
             SetList::ARRAY_STR_FUNCTIONS_TO_STATIC_CALL,
             SetList::DOCTRINE_CODE_QUALITY,
             SetList::CODING_STYLE,
