96c0d2f5c3b70f42e8a7354764d5b44cfc102c80
zingimmick <zingimmick@outlook.com>
2021-10-14 19:46:41 +0800

    Drop PHP 7.1 support

--- rector.php
+++ rector.php
@@ -14,7 +14,7 @@ use Zing\CodingStandard\Set\RectorSetList;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(RectorSetList::CUSTOM);
-    $containerConfigurator->import(LevelSetList::UP_TO_PHP_71);
+    $containerConfigurator->import(LevelSetList::UP_TO_PHP_72);
     $containerConfigurator->import(PHPUnitSetList::PHPUNIT_CODE_QUALITY);
     $containerConfigurator->import(SetList::TYPE_DECLARATION);
     $containerConfigurator->import(SetList::TYPE_DECLARATION_STRICT);
