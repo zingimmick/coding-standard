0978e998d18e84491fe13a483db94b1b51e6a85f
Zing <zingimmick@outlook.com>
2022-01-07 12:15:19 +0800

    Split printer (#121)

--- src/PHPCodeSnifferRuleSetGenerator.php
+++ src/PHPCodeSnifferRuleSetGenerator.php
@@ -7,25 +7,8 @@ namespace Zing\CodingStandard;
 use PHP_CodeSniffer\Config;
 use PHP_CodeSniffer\Ruleset;
 use PHP_CodeSniffer\Runner;
-use PhpParser\BuilderHelpers;
-use PhpParser\Node\Arg;
-use PhpParser\Node\Expr\Assign;
-use PhpParser\Node\Expr\ClassConstFetch;
-use PhpParser\Node\Expr\Closure;
-use PhpParser\Node\Expr\MethodCall;
-use PhpParser\Node\Expr\Variable;
-use PhpParser\Node\Name;
-use PhpParser\Node\Param;
-use PhpParser\Node\Scalar\LNumber;
-use PhpParser\Node\Stmt\Declare_;
-use PhpParser\Node\Stmt\DeclareDeclare;
-use PhpParser\Node\Stmt\Expression;
-use PhpParser\Node\Stmt\Nop;
-use PhpParser\Node\Stmt\Return_;
-use PhpParser\Node\Stmt\Use_;
-use PhpParser\Node\Stmt\UseUse;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\Finder\Finder;
+use Zing\CodingStandard\Printers\RuleSetPrinter;
 
 final class PHPCodeSnifferRuleSetGenerator
 {
@@ -45,65 +28,13 @@ final class PHPCodeSnifferRuleSetGenerator
     ];
 
     /**
-     * @var string
+     * @var \Zing\CodingStandard\Printers\RuleSetPrinter
      */
-    private const SERVICES = 'services';
+    private $printer;
 
-    private function printRuleSet(Ruleset $ruleset): string
+    public function __construct(RuleSetPrinter $printer)
     {
-        $sniffs = [];
-        foreach ($ruleset->sniffs as $sniff) {
-            $sniffs[is_object($sniff) ? get_class($sniff) : $sniff] = [];
-        }
-
-        foreach ($ruleset->ruleset as $code => $attr) {
-            if (isset($ruleset->sniffCodes[$code])) {
-                $sniff = $ruleset->sniffCodes[$code];
-
-                $sniffs[$sniff] = $attr['properties'] ?? [];
-            }
-        }
-
-        $services = [];
-        foreach ($sniffs as $sniff => $property) {
-            $expr = new MethodCall(new Variable(self::SERVICES), 'set', [
-                new Arg(new ClassConstFetch(new Name\FullyQualified($sniff), 'class')),
-            ]);
-            foreach ($property as $key => $value) {
-                $expr = new MethodCall($expr, 'property', [
-                    new Arg(BuilderHelpers::normalizeValue($key)),
-                    new Arg(BuilderHelpers::normalizeValue($value)),
-                ]);
-            }
-
-            $services[] = new Expression($expr);
-        }
-
-        array_unshift(
-            $services,
-            new Expression(new Assign(new Variable(self::SERVICES), new MethodCall(new Variable(
-                'containerConfigurator'
-            ), self::SERVICES)))
-        );
-
-        return (new Printer([
-            'shortArraySyntax' => true,
-        ]))
-            ->prettyPrintFile([
-                new Declare_([new DeclareDeclare('strict_types', new LNumber(1))]),
-                new Nop(),
-                new Use_([new UseUse(new Name(ContainerConfigurator::class))]),
-                new Nop(),
-                new Return_(new Closure([
-                    'static' => true,
-                    'returnType' => 'void',
-                    'params' => [
-                        new Param(new Variable('containerConfigurator'), null, new Name('ContainerConfigurator')),
-                    ],
-                    'stmts' => $services,
-                ])),
-                new Nop(),
-            ]);
+        $this->printer = $printer;
     }
 
     public function generate(): void
@@ -119,9 +50,30 @@ final class PHPCodeSnifferRuleSetGenerator
 
             file_put_contents(
                 sprintf(__DIR__ . '/../config/set/php_codesniffer/%s', self::MAP[$setDefinition->getRelativePath()]),
-                $this->printRuleSet($ruleset)
+                $this->printer->print($this->formatRulesToServices($ruleset))
             );
         }
+    }
+
+    /**
+     * @return array<string, array<string, mixed>>
+     */
+    private function formatRulesToServices(Ruleset $ruleset): array
+    {
+        $sniffs = [];
+        foreach ($ruleset->sniffs as $sniff) {
+            $sniffs[is_object($sniff) ? get_class($sniff) : $sniff] = [];
+        }
+
+        foreach ($ruleset->ruleset as $code => $attr) {
+            if (isset($ruleset->sniffCodes[$code])) {
+                $sniff = $ruleset->sniffCodes[$code];
+
+                $sniffs[$sniff] = $attr['properties'] ?? [];
+            }
+        }
+
+        return $sniffs;
     }
 
     /**
---
+++ src/Printers/PHPCodeSnifferRuleSetPrinter.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zing\CodingStandard\Printers;
+
+use PhpParser\Node\Expr;
+
+class PHPCodeSnifferRuleSetPrinter extends RuleSetPrinter
+{
+    public function configureService(Expr $expr, array $configuration): Expr
+    {
+        foreach ($configuration as $key => $value) {
+            $expr = $this->builderFactory->methodCall($expr, 'property', [$key, $value]);
+        }
+
+        return $expr;
+    }
+}
