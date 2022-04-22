d9137e4025a630d4eef9e51126a0481b8d6aff71
zingimmick <zingimmick@outlook.com>
2022-04-22 18:56:34 +0800

    Re-order rule sets

--- config/ecs.php
+++ config/ecs.php
@@ -8,12 +8,12 @@ use Zing\CodingStandard\Set\ECSSetList;
 
 return static function (ECSConfig $ecsConfig): void {
     $ecsConfig->sets([
-        SetList::PSR_12,
+        SetList::CLEAN_CODE,
         SetList::SYMPLIFY,
         SetList::COMMON,
-        SetList::CLEAN_CODE,
-        ECSSetList::PHP_CS_FIXER_CUSTOM,
+        SetList::PSR_12,
         ECSSetList::PHP_CODESNIFFER_CUSTOM,
         ECSSetList::SLEVOMAT_CODING_STANDARD_CUSTOM,
+        ECSSetList::PHP_CS_FIXER_CUSTOM,
     ]);
 };
--- config/rector.php
+++ config/rector.php
@@ -6,16 +6,17 @@ use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\SetList;
 use Zing\CodingStandard\Set\RectorSetList;
 
+
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->sets([
-        SetList::CODING_STYLE,
+        RectorSetList::FAKER_114,
         SetList::CODE_QUALITY,
         SetList::DEAD_CODE,
         SetList::PRIVATIZATION,
         SetList::NAMING,
-        SetList::EARLY_RETURN,
         SetList::TYPE_DECLARATION,
+        SetList::EARLY_RETURN,
         SetList::TYPE_DECLARATION_STRICT,
-        RectorSetList::FAKER_114,
+        SetList::CODING_STYLE,
     ]);
 };
